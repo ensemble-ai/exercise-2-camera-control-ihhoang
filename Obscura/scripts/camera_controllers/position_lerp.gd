e858bb8b96e1e2404a225be816b60ab4a57ba88d
ihhoang <156142939+ihhoang@users.noreply.github.com>
2024-11-01 00:00:35 -0700

    PositionLock+LerpSmooth

--- Obscura/scripts/camera_controllers/position_lerp.gd
+++ Obscura/scripts/camera_controllers/position_lerp.gd
@@ -1,5 +1,10 @@
-class_name PositionLock
+class_name PositionLerp
 extends CameraControllerBase
+
+
+@export var follow_speed:float = 0.05
+@export var catchup_speed:float = 0.1
+@export var leash_distance:float = 5.0
 
 func _ready() -> void:
 	super()
@@ -13,11 +18,24 @@ func _process(delta: float) -> void:
 	if draw_camera_logic:
 		draw_logic()
 	
-	global_position.x = target.global_position.x
-	global_position.z = target.global_position.z
+	var speed_mode = follow_speed
+	if target.velocity:
+		speed_mode = catchup_speed
 	
-	#print("curr_x: ", global_position.x)
-	#print("curr_z: ", global_position.z) 
+	var cpos = Vector3(global_position.x, 0, global_position.z)
+	var tpos = Vector3(target.global_position.x, 0, target.global_position.z)
+	
+	var distance = cpos.distance_to(tpos)	
+	var direction = (tpos - cpos).normalized()
+	#print("cpos: ", cpos, "; camera: ", global_position)
+	#print("tpos: ", tpos, "; target: ", target.global_position)
+	#print("distance: ", distance)
+	
+	if distance > leash_distance:
+		global_position += direction * (distance - leash_distance)
+	else:
+		var new_position = lerp(cpos, tpos, speed_mode)
+		global_position = Vector3(new_position.x, global_position.y, new_position.z)
 		
 	super(delta)
 
